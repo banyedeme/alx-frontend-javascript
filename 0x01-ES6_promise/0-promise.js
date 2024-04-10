c1794be003c0225e74e245a1e0f0f1e68d7151ab
banyedeme <banyedeme0@gmail.com>
2024-04-10 14:39:45 +0000

    Promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -4,16 +4,14 @@ function getResponseFromAPI(resolvePromise = true) {
     setTimeout(() => {
       if (resolvePromise) {
         const responseData = {
-          data: "Response from API",
-          status: 200
+          data: 'Response from API',
+          status: 200,
         };
         resolve(responseData); // Resolve the promise with the response data
       } else {
-        reject(new Error("Failed to fetch data from API")); // Reject the promise with an error
+        reject(new Error('Failed to fetch data from API')); // Reject the promise with an error
       }
     }, 2000); // Simulating a delay of 2 seconds
   });
 }
-
 module.exports = getResponseFromAPI;
-
