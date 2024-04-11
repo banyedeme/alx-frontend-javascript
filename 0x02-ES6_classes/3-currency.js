4e71bf6736e10ea946e368ca947e9d95a8365b6b
banyedeme <banyedeme0@gmail.com>
2024-04-11 17:31:33 +0000

    classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,39 @@
+class Currency {
+  constructor(code, name) {
+    this._code = '';
+    this._name = '';
+
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,80 @@
+class Currency {
+  constructor(code, name) {
+    this._code = '';
+    this._name = '';
+
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+}
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = 0;
+    this._currency = new Currency('', '');
+
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('Both amount and conversionRate must be numbers');
+  }
+}
+
+export { Currency, Pricing };
