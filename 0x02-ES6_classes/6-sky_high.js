5f8373fd3a4ab5ae3a849eaf69fda09af8e93f73
banyedeme <banyedeme0@gmail.com>
2024-04-12 15:30:43 +0000

    classes

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import { Building as BaseBuilding } from './5-building';
+
+class SkyHighBuilding extends BaseBuilding {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
+
+export default SkyHighBuilding;
