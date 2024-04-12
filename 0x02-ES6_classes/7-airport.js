07a3d996128f7b02a7cc67dba433a139264eb162
banyedeme <banyedeme0@gmail.com>
2024-04-12 15:34:54 +0000

    airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,36 @@
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  toString() {
+    return this._code;
+  }
+}
+
+export default Airport;
