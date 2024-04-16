5cfed55953d2a7bb1bf0a482f53d0e8bcf374ef3
banyedeme <banyedeme0@gmail.com>
2024-04-16 22:09:54 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,10 @@
-function createInt8TypedArray(length, position, value) {
-  if (position >= length || position < 0) {
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
     throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
-  const typedArray = new Int8Array(buffer);
-  typedArray[position] = value;
-
-  return buffer;
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
 }
-module.exports = { createInt8TypedArray };
