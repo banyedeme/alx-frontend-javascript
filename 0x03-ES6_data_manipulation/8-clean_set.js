5d1ab54d72ecb84201ca790ab870ae6c518415b6
banyedeme <banyedeme0@gmail.com>
2024-04-16 22:20:16 +0000

    code refactor

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,10 @@
-function cleanSet(set, startString) {
-  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
-  return filteredValues.join('-');
+export default function cleanSet(set, startString) {
+  if (!startString || !startString.length || typeof startString !== 'string') return '';
+
+  let finalString = '';
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) finalString += `${element.slice(startString.length)}-`;
+  });
+
+  return finalString.slice(0, finalString.length - 1);
 }
-module.exports = { cleanSet };
